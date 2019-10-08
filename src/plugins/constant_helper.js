36474811b8d3e035b4f33abe3991fb308e4355e6
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-10-08 10:05:54 +0800

    add vue app

--- src/plugins/constant_helper.js
+++ src/plugins/constant_helper.js
@@ -1,27 +1,27 @@
 export default {
     client_details: {
-        name: process.env.CLIENT_NAME || "User",
-        avatar: process.env.CLIENT_AVATAR || 'https://icon-library.net/images/my-profile-icon-png/my-profile-icon-png-3.jpg',
-        cover: process.env.CLIENT_COVER || 'https://www.larutadelsorigens.cat/wallpic/full/73-737840_amazing-nature-wallpaper-desktop-mobile-background-full-hd.jpg',
+        name: process.env.VUE_APP_CLIENT_NAME || "User",
+        avatar: process.env.VUE_APP_CLIENT_AVATAR || 'https://icon-library.net/images/my-profile-icon-png/my-profile-icon-png-3.jpg',
+        cover: process.env.VUE_APP_CLIENT_COVER || 'https://www.larutadelsorigens.cat/wallpic/full/73-737840_amazing-nature-wallpaper-desktop-mobile-background-full-hd.jpg',
         website: {
-            url: process.env.CLIENT_WEBSITE_URL,
-            label: process.env.CLIENT_WEBSITE
+            url: process.env.VUE_APP_CLIENT_WEBSITE_URL,
+            label: process.env.VUE_APP_CLIENT_WEBSITE
         },
         facebook: {
-            url: process.env.CLIENT_FACEBOOK_URL,
-            label: process.env.CLIENT_FACEBOOK
+            url: process.env.VUE_APP_CLIENT_FACEBOOK_URL,
+            label: process.env.VUE_APP_CLIENT_FACEBOOK
         }
     },
     home_header: {
-        logo: process.env.HOME_LOGO || 'https://i.stack.imgur.com/7JLVA.png',
-        label: process.env.HOME_LABEL || 'My Website'
+        logo: process.env.VUE_APP_HOME_LOGO || 'https://i.stack.imgur.com/7JLVA.png',
+        label: process.env.VUE_APP_HOME_LABEL || 'My Website'
     },
     chatbot: {
-        greetings: process.env.CHATBOT_GREETINGS,
-        appId: process.env.CHATBOT_APP_ID,
-        page_id: process.env.CHATBOT_PAGE_ID,
-        theme_color: process.env.CHATBOT_THEME_COLOR
+        greetings: process.env.VUE_APP_CHATBOT_GREETINGS,
+        appId: process.env.VUE_APP_CHATBOT_APP_ID,
+        page_id: process.env.VUE_APP_CHATBOT_PAGE_ID,
+        theme_color: process.env.VUE_APP_CHATBOT_THEME_COLOR
     },
-    name_display: process.env.APP_NAME || 'Application Name',
-    login_background: process.env.LOGIN_BACKGROUND || 'https://www.larutadelsorigens.cat/wallpic/full/73-737840_amazing-nature-wallpaper-desktop-mobile-background-full-hd.jpg'
+    name_display: process.env.VUE_APP_APP_NAME || 'Application Name',
+    login_background: process.env.VUE_APP_LOGIN_BACKGROUND || 'https://www.larutadelsorigens.cat/wallpic/full/73-737840_amazing-nature-wallpaper-desktop-mobile-background-full-hd.jpg'
 }
