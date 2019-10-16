94abf92e80be42b44db61a41828c7692014c846a
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-10-17 07:48:55 +0800

    update

--- src/store/modules/public_service.js
+++ src/store/modules/public_service.js
@@ -82,6 +82,31 @@ function initialState() {
                     id: 4
                 }
             ]
+        },{
+            app: "San Antonio - Quezon",
+            messages: [
+                {
+                    date_created: new Date(),
+                    message: 'Keep up the good work and more power . God bless . Thank you very much Mayor Erick Wagan !',
+                    likes: 421,
+                    id: 1
+                }, {
+                    date_created: new Date(),
+                    message: 'Maraming salamat po Mayor sa pagpapagawa nyo tricycle application para sa San Antonio, Quezon. Malaking tulong po ito para sa mga driver at pasahero. Godbless po Mayor Erick Wagan  !',
+                    likes: 532,
+                    id: 2
+                }, {
+                    date_created: new Date(),
+                    message: 'Thank po Mayor Erick Wagan for doing a great job in San Antonio, Quezon. Godbless you ! ',
+                    likes: 831,
+                    id: 3
+                }, {
+                    date_created: new Date(),
+                    message: 'GO MAYOR ERICK WAGAN,in everything you do,I am for you.Will continue praying for your well being.🙏🙏🙏',
+                    likes: 719,
+                    id: 4
+                }
+            ]
         }]
     }
 }
