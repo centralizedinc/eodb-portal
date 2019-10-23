f63b821bdfdee7e1b389b62ca28855832f62951c
Ariel <ariel.a.balita@gmail.com>
2019-10-23 10:39:35 +0800

    update introduction

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -2,6 +2,8 @@
   <div>
       <div class="textShadow" style="font-size: 24px; color:#fbf49d; width:100%">Ease of Doing Business Portal</div>
       <span style="margin-top:2vh ; color:#e4e2e2 " class="textShadow">Ease of Doing Business Portal of {{constant_helper.home_header.label}} is an online system that expedites the application process for business registration, in compliance with the Ease of Doing Business Act under Republic Act No. 11032.</span>
+      <p style="margin-top:2vh ; color:#e4e2e2 " class="textShadow">{{constant_helper.home_header.label}} created a unified business application form to make it easier for all our constituents to put up or renew businesses. It also features a zero-contact policy for complete transparency.</p>
+  
   </div>
 </template>
 
