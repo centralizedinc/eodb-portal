24ea61b2d8131599a75f0fdd8eb3fa2c1bf21c03
Ariel <ariel.a.balita@gmail.com>
2019-12-20 15:54:09 +0800

    update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -222,6 +222,21 @@
           <router-view></router-view>
         </a-col>
         <a-col :span="4" style="margin-left:1vw">
+          <a-card >
+            <span slot="title">
+              <a-icon type="folder"></a-icon>
+              My Documents
+            </span>
+            <a-card v-for="item in ePermits" :key="item.epermit_attachment" style="margin-top: 2px; text-align: center">
+              <pdf  :src="item.epermit_attachment" style="display: inline-block; width: 100%"></pdf>
+              <p style="font-weight:bold">{{item.business_no}}</p>
+              <span>{{item.business_details.business_name}}</span>
+            </a-card>
+            
+            
+            <!-- <a-table :columns="doc_col" :dataSource="documents"></a-table> -->
+          </a-card>
+
           <!-- <a-affix :offsetTop="40">
             <a-card
               :headStyle="{
@@ -328,7 +343,10 @@
 </template>
 
 <script>
+import pdf from 'vue-pdf'
+
 export default {
+  components:{pdf},
   data() {
     return {
       collapsed: false,
@@ -336,7 +354,8 @@ export default {
       visible: false,
       coordinates: { lat: 14.017685, lng: 121.417034 },
       animation: {},
-      selected_menu: [this.$route.fullPath]
+      selected_menu: [this.$route.fullPath],
+      documents:[{}]
     };
   },
   created() {
@@ -350,6 +369,7 @@ export default {
   methods: {
     init() {
       this.user = this.$store.state.user_session.user;
+      this.$store.dispatch('GET_BUSINESS_PERMIT')
       // console.log('USER_DETAILS ::: ', JSON.stringify(this.$store.state.user_session))
     },
     nav(e) {
@@ -401,7 +421,20 @@ export default {
       console.log("Clicked cancel button");
       this.visible = false;
     }
-  }
+  },
+    computed:{
+      ePermits(){
+        var permits = []
+        if(this.$store.state.permits.permits){
+            this.$store.state.permits.permits.forEach(permit=>{
+              if(permit.epermit_attachment){
+                permits.push(permit)
+              }
+          })
+        }        
+        return permits
+      }
+    }
 };
 </script>
 
