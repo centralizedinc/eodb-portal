f5f6eb63a4ff063054f8b250328328224cc4947a
Mark Quijom <markjhonpaul.quijom@gmail.com>
2020-01-16 10:51:17 +0800

    fix issue #11

--- src/views/admin/Collections.vue
+++ src/views/admin/Collections.vue
@@ -1,5 +1,10 @@
 <template>
-  <collection-card></collection-card>
+  <a-row>
+    <a-col :span="24">
+      <collection-card></collection-card>
+    </a-col>
+    <a-col :span="24"></a-col>
+  </a-row>
 </template>
 
 <script>
